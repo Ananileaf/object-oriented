d80f0fb24cf06f59d8afc7faddfbcb0566e97dee
Ananileaf <411787945@qq.com>
2016-02-24 14:20:03 +0800

    code finish

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -11,7 +11,7 @@ int main()
 	get_scanf.ToStringQueue(get_scanf.GetString());//调用函数输入 
 	/*这里说一下，为什么不直接输入字符串,而是调用了一个函数,是对原始字符串数据进行封装
 	 虽然本题意义不大，只是体现一下类的保密性而已*/
-	put_printf.que2=get_scanf.que;//本来这里也是，嫌麻烦就没弄了 
+	put_printf.que=get_scanf.que;//本来这里也是，嫌麻烦就没弄了 
 	put_printf.putqueue();//调用函数输出 
 	system("pause");
  } 
